e178db95cc3392c589c6b74d0c2ed6b16cd671c3
András Veres-Szentkirályi <vsza@vsza.hu>
2013-09-05 10:16:53 +0200

    implemented basic web interface

---
+++ events.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+from __future__ import unicode_literals
+from datetime import datetime
+from base64 import b64decode
+import os, json, ticket
+
+ISO_DATE_FMT = '%Y-%m-%d'
+
+class Event(object):
+    TEXT_KEYS = ('name', 'location', 'homepage', 'secret')
+    DATE_KEYS = ('start', 'end')
+
+    def __init__(self, eid, config):
+        self.eid = eid
+        for key in Event.TEXT_KEYS:
+            setattr(self, key, config[key])
+        for key in Event.DATE_KEYS:
+            setattr(self, key, datetime.strptime(config[key], ISO_DATE_FMT).date())
+
+    def generate_ticket(self, values):
+        vm = self.get_vending_machine()
+        ticket_id = ticket.encode(vm.generate())
+        if not os.path.exists(self.eid):
+            os.makedirs(self.eid)
+        with file(self.get_ticket_filename(ticket_id), 'wb') as ticket_file:
+            json.dump(values, ticket_file)
+        return ticket_id
+
+    def verify_ticket(self, ticket_id):
+        vm = self.get_vending_machine()
+        vm.verify(ticket.decode(ticket_id))
+        if not os.path.exists(self.get_ticket_filename(ticket_id)):
+            raise KeyError('Ticket file does not exist')
+
+    def get_ticket_filename(self, ticket_id):
+        return os.path.join(self.eid, ticket_id) + '.json'
+
+    def get_vending_machine(self):
+        return ticket.VendingMachine(b64decode(self.secret))
+
+
+def get(eid=None):
+    with file('config.json', 'rb') as config_file:
+        config = json.load(config_file)
+    if eid is None:
+        return [(eid, Event(eid, data)) for eid, data in config['events'].iteritems()]
+    else:
+        return Event(eid, config['events'][eid])
---
+++ web.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+from flask import Flask, render_template, abort, request, redirect, url_for
+from subprocess import check_output
+import events, re
+
+app = Flask(__name__)
+
+EMAIL_RE = re.compile(r'^[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$', re.I)
+TICKET_FIELDS = (
+        ('name', 'Name', bool),
+        ('email', 'E-mail', EMAIL_RE.match),
+        ('notes', 'Notes', None),
+        )
+
+@app.route('/')
+def event_list():
+    return render_template('event_list.html', event_list=events.get())
+
+@app.route('/<eid>.html', methods=['GET', 'POST'])
+def show_event(eid):
+    try:
+        event = events.get(eid)
+    except KeyError:
+        abort(404)
+    else:
+        values = {}
+        if request.method == 'POST':
+            all_fields_valid = True
+            for name, _, validator in TICKET_FIELDS:
+                value = request.form.get(name, '')
+                info = {'value': value}
+                if validator is not None and not validator(value):
+                    info['error'] = True
+                    all_fields_valid = False
+                values[name] = info
+            if all_fields_valid:
+                ticket = event.generate_ticket(values)
+                return redirect(url_for('show_ticket', eid=eid, ticket=ticket))
+        return render_template('show_event.html', event=event,
+                fields=TICKET_FIELDS, values=values)
+
+@app.route('/<eid>/<ticket>.html')
+def show_ticket(eid, ticket):
+    try:
+        event = events.get(eid)
+        event.verify_ticket(ticket)
+    except KeyError, AssertionError:
+        abort(404)
+    else:
+        ticket_ascii = check_output(['qrencode', '-t', 'ASCII', ticket])
+        return render_template('show_ticket.html', event=event,
+                ticket_id=ticket, ticket_ascii=ticket_ascii)
+
+if __name__ == '__main__':
+    app.run(debug=True)
